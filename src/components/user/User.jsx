d05c218e96d3b7d9e20711fe090c14bf8965a14e
omer7640 <139492515+omer7640@users.noreply.github.com>
2024-07-17 16:10:01 +0500

    Update User.jsx

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,3 +1,10 @@
-export function User() {
-  return <div>User</div>;
+export function User({ userData }) {
+  return (
+    <div>
+      <h2>
+        My name is {userData.name} I am {userData.age} years old and my role is
+        {userData.role}
+      </h2>
+    </div>
+  );
 }
